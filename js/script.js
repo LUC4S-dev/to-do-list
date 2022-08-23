12e06f2fba20d550385957c345660eccb7e8202e
Lucas Siqueira <lucaslenisbenicio@hotmail.com>
2022-08-23 10:48:35 -0300

    to-do list 1.0

--- js/script.js
+++ js/script.js
@@ -1,50 +1,56 @@
+
 let inputTask = document.querySelector(".newTask");
 let btnAddTask = document.querySelector(".btnAddTask");
 let tasks = document.querySelector(".tasks");
+let delBtn = document.querySelector(".delete");
 
+let li;
+let btnDel;
 
-
+// Function that creates a list item / Função que cria um li
 function createListItem(){
     let li = document.createElement('li');
     return li;
 }
 
-
+// This function creates a li based on input content / Essa função cria um li baseado no conteúdo do input
 function createInputText(inputText){
-    let li = createListItem();
+    li = createListItem();
     li.innerHTML = inputText;
     tasks.appendChild(li);
+    li.setAttribute('class', 'taskList');
     clearInput();
-    createDeleteBtn();
+    createDeleteBtn(li);
 
 }
-
+//This function just clear the input and assign focus / Essa função apenas limpa o input e atribui foco
 function clearInput(){
     inputTask.value = '';
     inputTask.focus();
 }
 
-function createDeleteBtn(){
+// Function that creates the delete button/ Função que cria o botão de apagar
+function createDeleteBtn(li){
+    li.innerHTML+= '  ';
+    let btnDel = document.createElement('button');
+    btnDel.innerHTML = 'Remover'
    
-    let delBtn = document.createElement('button');
-    delBtn.innerHTML = 'Remover'
-    tasks.appendChild(delBtn);
+    btnDel.setAttribute('class', 'delete');
+    btnDel.setAttribute('title', 'Apagar tarefa');
+    li.appendChild(btnDel);
 }
 
-//  function removeTask(){
-//      let delTask = inputTask.removeChild()
-//  }
-
+// Function under developmen / Função em desenvolvimento
+// function saveTasks(){
+//     const liTasks = tasks.querySelectorAll('li');
+//     console.log(liTasks);
+// }
 
 
 btnAddTask.addEventListener('click', function(event){
     if(!inputTask.value) return;
     event.preventDefault();
-    createInputText(inputTask.value);
-    
-    
-      
-       
+    createInputText(inputTask.value);     
 });
 
 inputTask.addEventListener('keypress', function(event){
@@ -54,4 +60,14 @@ inputTask.addEventListener('keypress', function(event){
    }
   
 })
+
+document.addEventListener('click', function(event){
+    let el = event.target;
+    
+    
+    if (el.classList.contains('delete')){
+       li.remove();
+    }
+   
+});
 
